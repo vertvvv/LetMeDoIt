7e7b34ec9f6a100aa7ebacea361210356b30db37
vertvvv <vertvvv@gmail.com>
2017-03-31 04:14:22 +0300

    Main page changed, idea page first steps added
    Main page design changed, dropdown profile menu made, idea page added

--- js/index.js
+++ js/index.js
@@ -2,3 +2,12 @@
  * Created by Ilya on 27.03.2017.
  */
 
+$('#profile-bar').on('click', function () {
+    $('.dropdown-menu').toggleClass('invisible-element');
+    $(this).toggleClass('active');
+});
+
+// $('#logo').on('click', function (e) {
+//     e.preventDefault();
+//     $('html,body').animate({scrollTop: $('body').offset().top}, 'slow');
+// });

79f222d950937d2a136ae53e93aab26ee33a3e57
vertvvv <vertvvv@gmail.com>
2017-03-31 20:39:37 +0300

    Idea page modified, main page changes
    Idea page changed, comments added, header shadow added, comments icon on
    main page added

--- js/index.js
+++ js/index.js
@@ -2,12 +2,26 @@
  * Created by Ilya on 27.03.2017.
  */
 
-$('#profile-bar').on('click', function () {
+$('#profile-bar').on('click', function (e) {
+    e.preventDefault();
     $('.dropdown-menu').toggleClass('invisible-element');
     $(this).toggleClass('active');
 });
 
-// $('#logo').on('click', function (e) {
-//     e.preventDefault();
-//     $('html,body').animate({scrollTop: $('body').offset().top}, 'slow');
-// });
+$('.mockup').on('click', function () {
+    let self = $(this).parent();
+
+    $('.img-grid__wrapper').each((i, item) => {
+        if (!$(item).is(self)) {
+            $(item).removeClass('mockup-full-width');
+            console.log('done', item);
+        }
+    });
+
+    $(this).parent().toggleClass('mockup-full-width');
+});
+
+$('#logo').on('click', function (e) {
+    e.preventDefault();
+    $('html,body').animate({scrollTop: $('body').offset().top}, 'slow');
+});

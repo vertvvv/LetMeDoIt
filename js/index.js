8717db0655fb1b2d75746eb187e34743dc3bf940
vertvvv <vertvvv@gmail.com>
2017-04-02 20:36:31 +0300

    "New idea" page completed, content fixed, scss refactored

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
  * Created by Ilya on 27.03.2017.
  */
 
-$('#profile-bar').on('click', function (e) {
+$('#profile-bar').on('click', function () {
     $('.dropdown-menu').toggleClass('invisible-element');
     $(this).toggleClass('active');
 });
@@ -13,7 +13,6 @@ $('.mockup').on('click', function () {
     $('.img-grid__wrapper').each((i, item) => {
         if (!$(item).is(self)) {
             $(item).removeClass('mockup-full-width');
-            console.log('done', item);
         }
     });
 
@@ -23,4 +22,22 @@ $('.mockup').on('click', function () {
 $('#logo').on('click', function (e) {
     e.preventDefault();
     $('html,body').animate({scrollTop: $('body').offset().top}, 'slow');
+});
+
+$('#addFunction').on('click', () => {
+    let input = `<input class="new-input">`;
+
+    $('#functionWrapper').append(input);
+    $('.new-input').fadeIn(1000);
+});
+
+$('#addWish').on('click', () => {
+    let wish = `<div class="wish new-input">
+                    <hr class="wish-hr">
+                    <input placeholder="Wish">
+                    <textarea placeholder="Description"></textarea>
+                </div>`;
+
+    $('#wishWrapper').append(wish);
+    $('.new-input').fadeIn(1000);
 });
